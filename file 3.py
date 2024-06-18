0550fc4440f9f9d839f2747c82d86b925932e422
Parnika <113688190+parnika-singh@users.noreply.github.com>
2024-06-18 20:50:49 +0530

    Add files via upload

---
+++ file 3.py
@@ -0,0 +1,5 @@
+# Declare a variable and store a real number (floating-point number) in it
+my_float = 3.14159
+
+# Print the value stored in the variable
+print("The stored real number is:", my_float)
