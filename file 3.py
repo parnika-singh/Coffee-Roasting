a4f06ac15a62a6396f568f98129dae7fa20f8fd1
Parnika <113688190+parnika-singh@users.noreply.github.com>
2024-06-19 23:24:29 +0530

    Delete file 3.py

--- file 3.py
+++
@@ -1,5 +0,0 @@
-# Declare a variable and store a real number (floating-point number) in it
-my_float = 3.14159
-
-# Print the value stored in the variable
-print("The stored real number is:", my_float)
